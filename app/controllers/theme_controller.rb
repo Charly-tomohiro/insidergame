5492161e803344df704d3c57118a73baa3174a72
小吹智広 <qwer1264106@gmail.com>
2020-05-10 14:33:53 +0900

    new role add on

--- app/controllers/theme_controller.rb
+++ app/controllers/theme_controller.rb
@@ -1,33 +1,11 @@
 class ThemeController < ApplicationController
-  def index
-    @rooms = Theme.all
-  end
+
+  before_action :set_theme_room, only: %i(show new02 new02update fase01think fase01updateTime fase01answer fase01update fase01answerBranch fase01good fase02think fase02answer fase02update fase02answerBranch fase02good fase01bad fase02bad)
  
   def show
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
-
-    seedArray = ["Insider", "Master"]
-    normalRoleArray=["Normal01","Normal02","Normal03","Normal04","Normal05","Normal06","Normal07"]
-    i = 2
-    if @theme.TagCheck == 1
-        seedArray.push("Tag1", "Tag2")
-        i = i+2
-    end
-    if @theme.CrazyCheck == 1
-        seedArray.push("Crazy")
-        i = i+1
-    end
-    if @theme.PeaceCheck == 1
-        seedArray.delete("Insider")
-        i = i-1
-    end
-    range = 0..@theme.Nop-i-1
-    range.each do |num|
-        seedArray.push(normalRoleArray[num])
-    end
-    @roleArray = seedArray
-
+    #01~08までの役割を明示する -->
+    @hashu = ["インサイダー（テーマ："+@theme.Title+"）", "マスター（テーマ："+@theme.Title+"）", "市民", "市民", "市民", "市民", "市民", "市民", "市民", "タッグ", "タッグ", "クレイジー"]
+    @user = [@room.User01Name, @room.User02Name, @room.User03Name, @room.User04Name, @room.User05Name, @room.User06Name, @room.User07Name, @room.User08Name]
   end
 
   def new
@@ -47,9 +25,6 @@ class ThemeController < ApplicationController
   end
 
   def new02
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
-    
     seedArray = ["Insider", "Master"]
     normalRoleArray=["Normal01","Normal02","Normal03","Normal04","Normal05","Normal06","Normal07"]
     i = 2
@@ -62,7 +37,10 @@ class ThemeController < ApplicationController
         i = i+1
     end
     if @theme.PeaceCheck == 1
-        seedArray.delete("Insider")
+        if rand(@theme.Nop) + 1 == 1
+          seedArray.delete("Insider")
+          seedArray.delete("Crazy")
+        end
         i = i-1
     end
     range = 0..@theme.Nop-i-1
@@ -70,12 +48,9 @@ class ThemeController < ApplicationController
         seedArray.push(normalRoleArray[num])
     end
     @roleArray = seedArray
-
   end
 
   def new02update #new02のpach
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
     if @theme.update_attributes(theme_params)
       redirect_to room_theme_url(@room, @theme)
     else
@@ -84,13 +59,9 @@ class ThemeController < ApplicationController
   end
 
   def fase01think #ファーズ01のゲーム中
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
   end
 
   def fase01updateTime #ファーズ01の時間ののpach
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
     if @theme.update_attributes(theme_params)
       redirect_to fase01answer_room_theme_url(@room, @theme)
     else
@@ -99,13 +70,9 @@ class ThemeController < ApplicationController
   end
 
   def fase01answer #ファーズ01の回答中
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
   end
 
   def fase01update #ファーズ01の回答のpost
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
     if @theme.update_attributes(theme_params)
       redirect_to fase01answerBranch_room_theme_url(@room, @theme)
     else
@@ -114,8 +81,6 @@ class ThemeController < ApplicationController
   end
 
   def fase01answerBranch #フェーズ1の正否の判断をする
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
     if @theme.Fase01Answer == @theme.Title
       redirect_to fase01good_room_theme_url(@room, @theme)
     else
@@ -124,24 +89,16 @@ class ThemeController < ApplicationController
   end
 
   def fase01good #ファーズ01の正解したとき
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
   end
 
   def fase02think #ファーズ02のゲーム中
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
     @fase02Time = 300 - @theme.Fase01Time.to_i
   end
 
   def fase02answer #ファーズ02の回答中
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
   end
 
   def fase02update #ファーズ02の回答のpost
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
     if @theme.update_attributes(theme_params)
       redirect_to fase02answerBranch_room_theme_url(@room, @theme)
     else
@@ -150,9 +107,6 @@ class ThemeController < ApplicationController
   end
 
   def fase02answerBranch #フェーズ2の正否の判断をする
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
-
     #回答の配列を作成
     answerArray = [@theme.Answer01,@theme.Answer02,@theme.Answer03,@theme.Answer04,@theme.Answer05,@theme.Answer06,@theme.Answer07,@theme.Answer08] 
     answerArrayNew = answerArray[0..@theme.Nop-1]
@@ -180,22 +134,22 @@ class ThemeController < ApplicationController
   end
 
   def fase02good #ファーズ02の正解したとき
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
   end
 
   def fase01bad #ファーズ02の正解したとき
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
   end
 
   def fase02bad #ファーズ02の正解したとき
-    @theme = Theme.find(params[:id])
-    @room = Room.find_by(id: params[:room_id])
   end
 
   private
     def theme_params
       params.require(:theme).permit(:room_id,:Insider,:Master,:Normal01,:Normal02,:Normal03,:Normal04,:Normal05,:Normal06,:Normal07,:Tag1,:Tag2,:Crazy,:Fase01Answer,:Answer01,:Answer02,:Answer03,:Answer04,:Answer05,:Answer06,:Answer07,:Answer08,:Fase01Time,:Title,:Nop,:PeaceCheck,:TagCheck,:CrazyCheck)
+    end
+
+    # ここにメソッドを追加する
+    def set_theme_room
+      @theme = Theme.find(params[:id])
+      @room = Room.find_by(id: params[:room_id])
     end
 end
